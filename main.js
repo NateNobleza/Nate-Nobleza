100d628874777989b36def2a07c3a49e3c10d0a0
Nate Nobleza <noblezanathan@gmail.com>
2024-06-15 05:44:14 +0000

    about me done

--- main.js
+++ main.js
@@ -1,59 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const desc1 = document.querySelector('.about-desc');
-  const chev1 = document.querySelector('.about-chev-right');
-  const desc2 = document.querySelector('.desc2');
-  const chev11 = document.querySelector('.chev11');
-  const chev22 = document.querySelector('.chev22');
-  const desc3 = document.querySelector('.desc3');
-  const chev111 = document.querySelector('.chev111');
-  const chev222 = document.querySelector('.chev222');
+document.addEventListener('DOMContentLoaded', function () {
+  const aboutChevRight = document.querySelector('.about-chev-right');
+  const aboutChevDown = document.querySelector('.about-chev-down');
+  const aboutDesc = document.querySelector('.about-desc');
 
-  // Hide desc1, desc2, and desc3 initially on page load
-  desc1.classList.add('hidden');
-  desc2.classList.add('hidden');
-  desc3.classList.add('hidden');
+  const techChevRight = document.querySelector('.tech-chev-right');
+  const techChevDown = document.querySelector('.tech-chev-down');
+  const techDesc = document.querySelector('.tech-desc');
 
-  if (chev1 && desc1) {
-    chev1.addEventListener('click', function() {
-      toggleVisibility(about-desc);
-      toggleVisibility(desc2);
-      toggleVisibility(desc3);
-      toggleChevronIcons(chev1, chev2);
-      toggleChevronIcons(chev11, chev22);
-      toggleChevronIcons(chev111, chev222);
+  const hobbyChevRight = document.querySelector('.hobby-chev-right');
+  const hobbyChevDown = document.querySelector('.hobby-chev-down');
+  const hobbyDesc = document.querySelector('.hobby-desc');
+
+  // Hide descriptions and down chevrons initially on page load
+  aboutDesc.classList.add('hidden');
+  techDesc.classList.add('hidden');
+  hobbyDesc.classList.add('hidden');
+  aboutChevDown.classList.add('hidden');
+  techChevDown.classList.add('hidden');
+  hobbyChevDown.classList.add('hidden');
+
+  // Function to toggle visibility of description and chevron icons
+  function toggleVisibilityAndChevron(descElement, chevRight, chevDown) {
+    descElement.classList.toggle('hidden');
+    chevRight.classList.toggle('chev-rotate');
+    chevDown.classList.toggle('chev-rotate');
+  }
+
+  // Event listeners for About section
+  if (aboutChevRight && aboutChevDown && aboutDesc) {
+    aboutChevRight.addEventListener('click', function () {
+      toggleVisibilityAndChevron(aboutDesc, aboutChevRight, aboutChevDown);
+    });
+
+    aboutChevDown.addEventListener('click', function () {
+      toggleVisibilityAndChevron(aboutDesc, aboutChevRight, aboutChevDown);
     });
   }
 
-  if (chev11 && desc2) {
-    chev11.addEventListener('click', function() {
-      toggleVisibility(desc1);
-      toggleVisibility(desc2);
-      toggleVisibility(desc3);
-      toggleChevronIcons(chev1, chev2);
-      toggleChevronIcons(chev11, chev22);
-      toggleChevronIcons(chev111, chev222);
+  // Event listeners for Technologies section
+  if (techChevRight && techChevDown && techDesc) {
+    techChevRight.addEventListener('click', function () {
+      toggleVisibilityAndChevron(techDesc, techChevRight, techChevDown);
+    });
+
+    techChevDown.addEventListener('click', function () {
+      toggleVisibilityAndChevron(techDesc, techChevRight, techChevDown);
     });
   }
 
-  if (chev111 && desc3) {
-    chev111.addEventListener('click', function() {
-      toggleVisibility(desc1);
-      toggleVisibility(desc2);
-      toggleVisibility(desc3);
-      toggleChevronIcons(chev1, chev2);
-      toggleChevronIcons(chev11, chev22);
-      toggleChevronIcons(chev111, chev222);
+  // Event listeners for Hobbies section
+  if (hobbyChevRight && hobbyChevDown && hobbyDesc) {
+    hobbyChevRight.addEventListener('click', function () {
+      toggleVisibilityAndChevron(hobbyDesc, hobbyChevRight, hobbyChevDown);
+    });
+
+    hobbyChevDown.addEventListener('click', function () {
+      toggleVisibilityAndChevron(hobbyDesc, hobbyChevRight, hobbyChevDown);
     });
   }
 });
-
-function toggleVisibility(element) {
-  element.classList.toggle('hidden');
-}
-
-function toggleChevronIcons(icon1, icon2) {
-  if (icon1 && icon2) {
-    icon1.classList.toggle('hidden');
-    icon2.classList.toggle('hidden');
-  }
-}
